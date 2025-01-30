27184d69b00be221c6cb61bf328f6e34c8fc6678
Fede <fredez1991@gmail.com>
2025-01-29 23:47:22 -0300

    futsal for her v2.0

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import adminRouter from './routes/adminRouter.js'
 import authRouter from './routes/authRouter.js'
+import publicTournamentRouter from './routes/publicTournamentRouter.js'
 
 export const router = express.Router()
+router.use('/tournaments', publicTournamentRouter)
 router.use('/admin', adminRouter)
 router.use('/auth', authRouter)
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import runValidations from '../middlewares/common/validations/runValidations.js'
-import tournamentRouter from './tournamentRouter.js'
 import teamRouter from './teamRouter.js'
 import fixtureRouter from './fixture/fixtureRouter.js'
 import validateToken from '../middlewares/user/validations/validateToken.js'
 import validateRole from '../middlewares/user/validations/validateRole.js'
 import verifyUserToken from '../middlewares/user/validations/verifyUserToken.js'
 import { checkTokenAndRoleDb } from '../middlewares/user/validations/checkTokenAndRoleDb.js'
+import priviteTournamentRouter from './priviteTournamentRouter.js'
 
 const adminRouter = express.Router()
 
@@ -16,7 +16,7 @@ const runValidateAdminValues = runValidations([
 ])
 
 adminRouter.use(runValidateAdminValues, verifyUserToken, checkTokenAndRoleDb)
-adminRouter.use('/tournament-details', tournamentRouter)
+adminRouter.use('/tournament-details', priviteTournamentRouter)
 adminRouter.use('/teams', teamRouter)
 adminRouter.use('/fixture', fixtureRouter)
 
