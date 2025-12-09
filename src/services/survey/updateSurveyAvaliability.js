c49c06d39a7bce4bc8a1d54a2019c1a0c9d34a5b
Fede <fredez1991@gmail.com>
2025-12-09 21:42:16 +0100

    little fix survey

--- src/services/survey/updateSurveyAvaliability.js
+++ src/services/survey/updateSurveyAvaliability.js
@@ -6,24 +6,53 @@ const { ERROR_WHILE_UPDATING_SURVEY } = errorCodes.mvpErrors
 
 const updateSurveyAvaliability = async(req, res) => {
     const MVPSurvey = req.body.MVPSurvey
+
     const mvpSurveyId = MVPSurvey.mvpSurveyId
     const votingIsAvaliable = MVPSurvey.votingIsAvaliable
+    const playerId = MVPSurvey.playerId
+    const showMVP = MVPSurvey.showMVP || false
 
-    let updatedFields
+    let updatedFields = {}
+
+    // Caso 1: votingIsAvaliable = false
+    if (!votingIsAvaliable) {
+        updatedFields = {
+            votingIsAvaliable,
+            showMVP
+        }
+    }
+
+    // Caso 2: votingIsAvaliable = true y NO hay playerId
+    if (votingIsAvaliable && !playerId) {
+        updatedFields = {
+            votingIsAvaliable
+        }
+    }
+
+    // Caso 3: votingIsAvaliable = true y hay playerId
+    if (!votingIsAvaliable && playerId) {
+        updatedFields = {
+            votingIsAvaliable,
+            playerId
+        }
+    }
+
+    // Caso 4: votingIsAvaliable = true y hay showMVP
     if (votingIsAvaliable) {
-        const showMVP = MVPSurvey.showMVP || false
-        updatedFields = { votingIsAvaliable, showMVP }
+        updatedFields = {
+            votingIsAvaliable,
+            showMVP
+        }
     }
 
-    if (MVPSurvey.playerId) {
-        updatedFields = { votingIsAvaliable, playerId: MVPSurvey.playerId }
-    }
     try {
         await surveyService.updateMVPSurvey(mvpSurveyId, { updatedFields })
 
         return res.status(StatusCodes.OK).json({})
     } catch (err) {
-        return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: [{ msg: ERROR_WHILE_UPDATING_SURVEY }] })
+        return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+            errors: [{ msg: ERROR_WHILE_UPDATING_SURVEY }]
+        })
     }
 }
 
